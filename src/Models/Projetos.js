6b9f7c29f8a95ef3ab37cf7394959e62bf00a398
Matheus Cruz <165737916+rlxmts@users.noreply.github.com>
2024-09-11 10:48:47 -0300

    :wrench: Configuração: Adicionando 'repositorio' e 'deploy' ao Schema como obrigatorio

--- src/Models/Projetos.js
+++ src/Models/Projetos.js
@@ -4,6 +4,8 @@ const projetoSchema = new mongoose.Schema({
   id: {type: mongoose.Schema.Types.ObjectId},
   imagem: {type: String, required: true},
   titulo: {type: String, required:true},
+  repositorio: {type: String, required:true},
+  deploy: {type: String, required: true},
   tags: {type: Array},
   data: {
     type: Date,
